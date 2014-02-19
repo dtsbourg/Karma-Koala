970ead400544fb902db000667e47edac8831986d
dtsbourg <dylan.bourgeois@epfl.ch>
2014-02-19 16:34:28 +0100

    Initial Commit

---
+++ RoomieParse/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RoomieParse
+//
+//  Created by Dylan Bourgeois on 19/02/14.
+//  Copyright (c) 2014 Dylan Bourgeois. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RoomieParse/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RoomieParse
+//
+//  Created by Dylan Bourgeois on 19/02/14.
+//  Copyright (c) 2014 Dylan Bourgeois. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
